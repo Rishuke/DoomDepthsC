c1f466add6df65755fafab8a06b0947eecf079ce
meizarkos <aduplessis1@myges.fr>
2023-10-01 02:17:08 +0200

    StartMapCreation

--- creationmap.c
+++ creationmap.c
@@ -5,6 +5,7 @@
 #include <SDL/SDL_image.h>      // SDL_image extension for image loading
 #include <SDL/SDL_rotozoom.h>
 #include <SDL/SDL_mixer.h>
+#define taille 5
 
 SDL_Surface* initSDL(){
     if (SDL_Init(SDL_INIT_VIDEO) != 0) { //SDL OK
@@ -42,7 +43,7 @@ void freeAll(int size,int** toFree){
 }
 
 void initImg(SDL_Surface **img){ //initialisation des pointeurs sans malloc
-    img[0]=IMG_Load("../images/piece.jpg"); //piece
+    /**img[0]=IMG_Load("../images/piece.jpg"); //piece
     img[0]=rotozoomSurfaceXY(img[0],0.0,0.22,0.22,1);
     SDL_SetColorKey(img[0],SDL_SRCCOLORKEY,SDL_MapRGB(img[0]->format,255,255,255));
     if(!img[0])exit(1);
@@ -67,12 +68,48 @@ void initImg(SDL_Surface **img){ //initialisation des pointeurs sans malloc
     img[6]= IMG_Load("../images/BOSS.jpg"); //Boss a la fin des chemins
     img[6]=rotozoomSurfaceXY(img[6],0.0,0.22,0.22,1);
     SDL_SetColorKey(img[6],SDL_SRCCOLORKEY,SDL_MapRGB(img[6]->format,255,255,255));
-    if(!img[6])exit(1);
+    if(!img[6])exit(1);**/
 }
 
-void initMap(int** map){ //0=interdit 1=piece 2=mob 3=rien 4=porte 5=joueur 6=shop
-    
-    int xJoueur=
+void initMap(int** map){ //0=interdit 1=piece 2=mob 3=rien 4=porte 5=joueur 6=shop 7=boss
+    srand(time(NULL));
+    int xPlayer=rand()%taille;
+    int yPlayer=rand()%taille;
+    map[xPlayer][yPlayer]=5;
+    int xShop=rand()%taille;
+    int yShop=rand()%taille;
+    while(map[xShop][yShop]!=0){
+        xShop=rand()%taille;
+        yShop=rand()%taille;
+    }
+    map[xShop][yShop]=6;
+    int xExit=rand()%taille;
+    int yExit=rand()%taille;
+    while(map[xExit][yExit]!=0){
+        xExit=rand()%taille;
+        yExit=rand()%taille;
+    }
+    map[xExit][yExit]=4;
+    int xBoss1=rand()%taille;
+    int yBoss1=rand()%taille;
+    while(map[xBoss1][yBoss1]!=0){
+        xBoss1=rand()%taille;
+        yBoss1=rand()%taille;
+    }
+    map[xBoss1][yBoss1]=7;
+    int xBoss2=rand()%taille;
+    int yBoss2=rand()%taille;
+    while(map[xBoss2][yBoss2]!=0){
+        xBoss2=rand()%taille;
+        yBoss2=rand()%taille;
+    }
+    map[xBoss2][yBoss2]=7;
+    for(int i=0;i<taille;i++){
+        for(int j=0;j<taille;j++){
+            printf("%d ",map[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int initTest(){ //equivalent du main
@@ -93,7 +130,7 @@ int initTest(){ //equivalent du main
     SDL_Event event;
     int continuer=1;
     SDL_EnableKeyRepeat(10,10);
-    while(continuer)
+    /**while(continuer)
     {
         SDL_WaitEvent(&event);
         switch(event.type)
@@ -113,7 +150,7 @@ int initTest(){ //equivalent du main
         FondGlob.y=500;
         SDL_BlitSurface(Img[2], NULL, ecran, &FondGlob);
         SDL_Flip(ecran);
-    }
+    }**/
     //sauvegarde file
 
     //Tout fermer
--- creationmap.h
+++ creationmap.h
@@ -7,5 +7,6 @@
 int initTest(); //fonction useless
 void initImg(SDL_Surface **img);
 void closeAllSDL();
+void initMap(int** map);
 SDL_Surface* initSDL();
 #endif //DOOMDEPTHSC_CREATIONMAP_H
