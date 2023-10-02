55fff5add199a11191df8371e681ebb8f0ce4733
meizarkos <aduplessis1@myges.fr>
2023-10-02 21:21:30 +0200

    Fonction Basique

---
+++ StartEndGame.h
@@ -0,0 +1,10 @@
+//
+// Created by meizarkos on 02/10/23.
+//
+
+#ifndef DOOMDEPTHSC_STARTENDGAME_H
+#define DOOMDEPTHSC_STARTENDGAME_H
+SDL_Surface* initSDL();
+void closeAllSDL();
+void freeAll(int size,int** toFree);
+#endif //DOOMDEPTHSC_STARTENDGAME_H
