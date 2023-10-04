8228dbf8f4bf7a4aaa3be31e442b6bcdd0eeb4b5
Toureg <ced.sahel@gmail.com>
2023-10-04 20:52:05 +0200

    struct

---
+++ struct.h
@@ -0,0 +1,37 @@
+typedef struct
+{
+    int equiped;
+    char *name;
+    int offensive;
+    int power;
+    int gold;
+} Item;
+
+typedef struct
+{
+    char *name;
+    int hp;
+    int attack;
+    int gold;
+    int mana;
+    int defense;
+    Item** items;
+    int x;
+    int y;
+    int xp;
+    int level;
+    int xpForNextLvl;
+} Player;
+
+typedef struct
+{
+    int hp;
+    char *name;
+    int attackMin;
+    int attackMax;
+    int defense;
+    int xpEarn;
+
+} Monster;
+
+
